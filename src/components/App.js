c498005097450e4f91dc19c49e064b7f86aa2464
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-10-22 14:39:16 -0400

    render sushi and more sushi button function

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+      .then (r => r.json())
+      .then (sushis => {
+        setSushis(sushis)
+      })
+  }, [])
+
+  function handleEatSushi(eatenSushi) {
+    const updatedSushis = sushis.map((sushi) => {
+      if (sushi.id === eatenSushi.id) return {...sushi}
+      return sushi
+    })
+
+    setSushis(updatedSushis)
+  }
+
+
+  
+
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer 
+        sushis={sushis} 
+        onEatSushi = {handleEatSushi}
+      />
       <Table />
     </div>
   );
--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({sushi, onEatSushi}) {
+  const {name, img_url, price, eaten} = sushi
+
+  function handleClick () {
+    onEatSushi(sushi)
+  }
+    
+  
+
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
+      <div className="plate" onClick={handleClick}>
         {/* Tell me if this sushi has been eaten! */}
         {false ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={img_url}
+            alt={name}
             width="100%"
           />
         )}
